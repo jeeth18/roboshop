7087512c1945bbc230b5432bc20da165536fcc92
jeeth18 <jeethsharma6@gmail.com>
2023-09-06 19:53:16 +0530

    frontend

---
+++ catalogue.sh
@@ -0,0 +1,10 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+cp /etc/systemd/system/catalogue.service.sh
